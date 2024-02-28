274249feef32f742e4d1393e934be134db2ac2a0
Sahl Hafidz <akunbelajarhafidz@gmail.com>
2024-02-28 13:03:33 +0700

    done

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const apiKey = 'a6ab08ce4391df9af7a8c3ea7dc339f6';
-const city = 'Semarang';
+function kirim(){
+  let pilihan_kota=document.getElementById('pilih_kota').value;
+  const city = pilihan_kota;
+  const apiKey = 'a6ab08ce4391df9af7a8c3ea7dc339f6';
+
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-
-
-fetch(apiUrl)
+if(pilihan_kota){
+  fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
     const weatherInfo = `
@@ -18,4 +20,11 @@ fetch(apiUrl)
   .catch(error => {
     console.error('Error fetching weather data:', error);
     document.querySelector('.weather-info').innerHTML = '<p>Failed to fetch weather data. Please try again later.</p>';
-  });
+  });  
+}
+
+
+}
+
+
+
